bdde15e5c16023cbbc20c20572f26bffa3f89b5e
Teriyaki-Kun <michhu1212@gmail.com>
2025-11-20 18:35:14 +0700

    huehue

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask,jsonify, request
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+loaded_model = joblib.load("models/model_diabetes.pkl")
+
+loaded_scaler = joblib.load("models/standard_scaler.pkl")
+
+columns = [
+        'Pregnancies', 
+        'Glucose', 
+        'BloodPressure', 
+        'SkinThickness', 
+        'Insulin', 
+        'BMI', 
+        'DiabetesPedigreeFunction', 
+        'Age'
+    ]
+
+@app.route('/')
+def index():
+    return jsonify({
+        "meta" : {
+            "status" : "success",
+            "message" : "Welcome to Diabetes"
+        },
+        "data" : None
+    })
+
+@app.route ('/api/predict', methods = {"POST"})
+def predict():
+    data = request.get_json()
+
+    X_input = pd.DataFrame([data], columns=columns)
+
+    X_input_scaled = loaded_scaler.transform (X_input)
+
+    prediction = loaded_model.predict(X_input_scaled)
+
+    return jsonify({
+        "meta": {
+            "status" : "Success",
+            "message" : "Prediction"
+        },
+        "data" : "Positive" if prediction.tolist()[0] == 1 else "Negative"
+    })
+
+if __name__ == '__main__': 
+    app.run(debug = True)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from if31-flask-python!")
+
+
+if __name__ == "__main__":
+    main()
